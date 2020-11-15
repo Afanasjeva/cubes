ac9d5bf193b05241a521da8200bdadce15440580
Tatjana Afanasjeva <tatjana.afanasjeva@gmail.com>
2020-11-15 15:05:07 +0200

    gh-pages

---
+++ precache-manifest.6340db8849cfbd52deca95d91a24db53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa9b67583586804dc5b",
+    "url": "/cubes/css/app.d1cd1d3d.css"
+  },
+  {
+    "revision": "d047c524c2a26614af5e",
+    "url": "/cubes/css/chunk-vendors.3fb60f33.css"
+  },
+  {
+    "revision": "75ca42c24a6987483a4cfe828cbf97fa",
+    "url": "/cubes/index.html"
+  },
+  {
+    "revision": "4fa9b67583586804dc5b",
+    "url": "/cubes/js/app.25101a1f.js"
+  },
+  {
+    "revision": "d047c524c2a26614af5e",
+    "url": "/cubes/js/chunk-vendors.b6846385.js"
+  },
+  {
+    "revision": "b5329ffce217a87b47be10d25482859e",
+    "url": "/cubes/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cubes/robots.txt"
+  }
+]);
